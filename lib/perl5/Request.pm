ba92aede98d920e8f2bec6fe2ae1d86d0de59d62
Fairfax Smartware <info@fairfaxsmartware.com>
2018-07-18 13:42:30 -0400

    added SSL tuning and ability to write-to-file

--- lib/perl5/Request.pm
+++ lib/perl5/Request.pm
@@ -1,10 +1,12 @@
 package Request;
 
 use strict;
+use warnings;
 use Response;
 use LWP::UserAgent;
 use HTTP::Request;
 use HTTP::Cookies;
+use IO::Socket::SSL;
 use JSON;
 
 use Class::MethodMaker [
@@ -14,8 +16,24 @@ use Class::MethodMaker [
 
 sub init {
     my ($self, $args) = @_;
-    $self->ua(new LWP::UserAgent);
+    
+    if ($args->{cacert} || $args->{cert} || $args->{key}) {
+        $self->ua(new LWP::UserAgent( ssl_opts => {
+            SSL_use_cert    => 1,
+            SSL_version     => $args->{SSL_version} || 'TLSv12',
+            SSL_verify_mode => $args->{SSL_VERIFY_PEER} || IO::Socket::SSL_VERIFY_PEER,
+            SSL_ca_file     => $args->{cacert},
+            SSL_cert_file   => $args->{cert},
+            SSL_key_file    => $args->{key}
+        }));
+    }
+    else {
+        $self->ua(new LWP::UserAgent);
+    }
+
     $self->ua->cookie_jar(new HTTP::Cookies);
+
+    
 }
 
 sub raw {
@@ -24,6 +42,10 @@ sub raw {
     my $headers = $args->{headers} = $args->{headers} || [];
 
     my $data = $args->{data};
+
+    if ($args->{tofile}) {
+        return $self->ua->request(new HTTP::Request($method, $url, $headers, $data), ':content_file' => $args->{tofile}, ':read_size_hint' => 8192);
+    }
 
     return $self->ua->request(new HTTP::Request($method, $url, $headers, $data));
 }    
